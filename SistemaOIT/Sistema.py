255b3a5cb76da830ceeb11e9531252ab58e95716
Rafael Moccelin Lavratti <theblockgamesbr@gmail.com>
2023-12-28 08:37:25 -0300

    Modificações

--- SistemaOIT/Sistema.py
+++ SistemaOIT/Sistema.py
@@ -1,8 +1,41 @@
 import tkinter as tk 
 from tkinter import messagebox
 
+#Janela
+janela = tk.Tk()
+#Define o tamanho da Janela
+janela.geometry("600x600")
 
-#Função para verificar login
-def verificar_login():
-    usuario = entry_usuario.get()
 
+
+
+
+
+
+#Verificar se o login e senha são válidos
+def login_funcao():
+    #verifica se os campos login e senha estão preenchidos
+    if login.get() == "" or senha.get() == "":
+        tk.messagebox.showerror("Erro", "Preencha todos os campos.")
+        return
+    #Verifique se o login e a senha são válidos
+    if login.get() == "admin" and senha.get() == "admin":
+        tk.messagebox.showerror("Sucesso!", "Login bem sucessido")
+        janela.destroy()
+    else:
+        tk.messagebox.showerror("Erro", "Login ou senha invalidos")
+        
+        #Botao de Login
+botao_login = tk.Button(janela, text="Login", command=login_funcao)
+
+
+#Campo de login e senha
+login = tk.Entry(janela)
+senha = tk.Entry(janela, show="*")
+
+#Posições
+login.grid(row=0, column=0)
+senha.grid(row=1, column=0)
+botao_login.grid(row=4, column=0)
+
+janela.mainloop()
